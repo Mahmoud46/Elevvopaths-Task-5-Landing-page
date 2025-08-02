9d35983381ce9a8e5b2119ff39800ce2d314e220
mahmoudzakaria12 <mahmoudzsali247@gmail.com>
2025-08-02 11:58:57 +0300

    Update toggle theme

--- src/libs/utils.ts
+++ src/libs/utils.ts
@@ -1,9 +1,3 @@
-export const toggleTheme = () => {
-	const currentTheme = document.documentElement.getAttribute("data-theme");
-	const nextTheme = currentTheme === "dark" ? "light" : "dark";
-	applyTheme(nextTheme);
-};
-
 export const applyTheme = (theme: "light" | "dark") => {
 	localStorage.setItem("data-theme", theme);
 	document.documentElement.setAttribute("data-theme", theme);
